dc44c0df5865ccbfe67a2a792db53d08078175c8
cnpatton <botanicality@gmail.com>
2018-03-21 20:06:22 -0500

    Added Shotgun art in and blast animators.

--- ThunderPaws2D/Assets/Scripts/WeaponScripts/FuzzBuster.cs
+++ ThunderPaws2D/Assets/Scripts/WeaponScripts/FuzzBuster.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlasmaRifle : AbstractWeapon {
+public class FuzzBuster : AbstractWeapon {
     /// <summary>
     /// Indicates the player is holding down the fire button
     /// </summary>
--- ThunderPaws2D/Assets/Scripts/WeaponScripts/Shotgun.cs
+++ ThunderPaws2D/Assets/Scripts/WeaponScripts/Shotgun.cs
@@ -14,6 +14,11 @@ public class Shotgun : AbstractWeapon {
 
     private Vector2[] _debugBlastRotations = new Vector2[7];
 
+    /// <summary>
+    /// Sprite animatio to play when the bullet impacts
+    /// </summary>
+    public Transform ImpactEffect;
+
     private void Update() {
 
         for (var i = 1; i < _debugBlastRotations.Length; ++i) {
@@ -40,6 +45,7 @@ public class Shotgun : AbstractWeapon {
             // Generate 5 raycasts 45, 22.5, 0, 337.5, 315 degrees
             // Rotation around the z axis
             var i = 1;
+            var collisionMap = new Dictionary<string, Collider2D>();
             foreach(var rotation in _blastRotations) {
                 var finalRotation = rotation * (Player.FacingRight ? 1 : -1);
 
@@ -64,13 +70,28 @@ public class Shotgun : AbstractWeapon {
                     xDir*Mathf.Sin(rads) + yDir*Mathf.Cos(rads)
                     );
 
-                //Store bullet origin spawn point (A)
+                // Store bullet origin spawn point (A)
                 Vector2 firePointPosition = new Vector2(FirePoint.position.x, FirePoint.position.y);
                 _debugBlastRotations[0] = firePointPosition;
-                //Collect the hit data - distance and direction from A -> B
+                // Collect the hit data - distance and direction from A -> B
                 RaycastHit2D shot = Physics2D.Raycast(firePointPosition, rotatedDirection, _rayLength, WhatToHit);
+
+                // Check if it hit anything
+                //We want to allow bullets to pass throught obstacles that the player can pass through
+                if (shot.collider != null) {
+                    print("We have collided with something i: " + i);
+                    Collider2D outCollider;
+                    if (!collisionMap.TryGetValue(shot.collider.gameObject.name, out outCollider)) {
+                        collisionMap.Add(shot.collider.gameObject.name, shot.collider);
+                    }
+                }
+
                 _debugBlastRotations[i] = rotatedDirection;
                 ++i;
+            }
+
+            foreach(var collider in collisionMap) {
+                HitTarget(collider.Value);
             }
 
             TimeToSpawnEffect = Time.time + 1 / EffectSpawnRate;
@@ -78,6 +99,38 @@ public class Shotgun : AbstractWeapon {
                 Ammo -= 1;
             }
         }
+    }
+
+    /// <summary>
+    /// Destroy and generate effects
+    /// </summary>
+    /// <param name="hitObject"></param>
+    private void HitTarget(Collider2D hitObject) {
+        //Damage whoever we hit - or rocket jump
+        Player player;
+        if (hitObject.gameObject.tag == GameConstants.Tag_Player) {
+            Debug.Log("We hit " + hitObject.name + " and did " + Damage + " damage");
+            player = hitObject.GetComponent<Player>();
+            if (player != null) {
+                //player.DamageHealth(Damage);
+            }
+        }
+        print("Hit object: " + hitObject.gameObject.tag);
+        //IF we hit a lifeform damage it - otherwise move on
+        var lifeform = hitObject.transform.GetComponent<BaseLifeform>();
+        if (lifeform != null) {
+            print("hit lifeform: " + lifeform.gameObject.name + " and did " + Damage + " damage");
+            lifeform.Damage(Damage);
+        }
+        GenerateEffect(hitObject.transform);
+    }
+
+    private void GenerateEffect(Transform impactTransform) {
+        // Subtract 1 from whatever the Z position is to ensure the explosion animation is played over the top
+        var fixedPosition = new Vector3(impactTransform.position.x, impactTransform.position.y, impactTransform.position.z - 1);
+        var clone = Instantiate(ImpactEffect, fixedPosition, impactTransform.rotation);
+        clone.GetComponent<DeathTimer>().TimeToLive = 0.25f;
+        clone.GetComponent<Animator>().SetBool("Invoke", true);
     }
 
     protected override void GenerateShot(Vector3 shotPos, Vector3 shotNormal, LayerMask whatToHit, string layer, bool ultMode, float freeFlyDelay = 0.5F) {
