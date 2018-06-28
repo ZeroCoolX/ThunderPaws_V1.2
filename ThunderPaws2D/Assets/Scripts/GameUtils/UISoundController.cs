d23c134980445037392a291b2864758b08972580
cnpatton <botanicality@gmail.com>
2018-06-27 21:03:26 -0500

    Added audio for the menus

---
+++ ThunderPaws2D/Assets/Scripts/GameUtils/UISoundController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISoundController : MonoBehaviour {
+
+    public string SoundName;
+    private Button _button { get { return GetComponent<Button>(); } }
+    private bool _joystickReset = true;
+
+	// Use this for initialization
+	void Start () {
+        _button.onClick.AddListener(() => PlaySound());
+	}
+
+    private void Update() {
+        if(JoystickManagerController.Instance.ConnectedControllers() == 0) {
+            if (Input.GetButtonDown("Universal_Vertical") || Input.GetButtonDown("Universal_Horizontal")) {
+                AudioManager.Instance.playSound("MenuButton_Navigate");
+            }
+        }else {
+            if (!_joystickReset) {
+                _joystickReset = Input.GetAxisRaw("Universal_Horizontal") == 0 && Input.GetAxisRaw("Universal_Vertical") == 0;
+            }
+            if (Input.GetAxisRaw("Universal_Vertical") > 0.8 || Input.GetAxisRaw("Universal_Vertical") < -0.8
+                || Input.GetAxisRaw("Universal_Horizontal") > 0.8 || Input.GetAxisRaw("Universal_Horizontal") < -0.8) {
+                if (_joystickReset) {
+                    _joystickReset = false;
+                    print("playing sound");
+                    AudioManager.Instance.playSound("MenuButton_Navigate");
+                }
+            }
+        }
+    }
+
+    void PlaySound() {
+        AudioManager.Instance.playSound(SoundName);
+    }
+}
