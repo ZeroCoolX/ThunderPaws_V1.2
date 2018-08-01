7946d205d9a8a0d612e8eadffa4656953231eb05
cnpatton <botanicality@gmail.com>
2018-08-01 17:07:21 -0500

    Refactored and cleanedup Baddie Ground 1

--- ThunderPaws2D/Assets/Scripts/Lifeforms/Baddie/Robot_GL1.cs
+++ ThunderPaws2D/Assets/Scripts/Lifeforms/Baddie/Robot_GL1.cs
@@ -4,28 +4,13 @@ using System.Linq;
 using UnityEngine;
 
 public class Robot_GL1 : GroundBaddieLifeform {
-    // These are the baddie specific values for properties that live on the parents
-    // This could be extracted out into configs but I don't mind them living here
-    // They're just used for initializing the property values that live on the 
-    // parent classes
-    private readonly float _gravity = -25.08f;
-    private readonly float _health = 5f;
-    private readonly float _shotDelay = 3f;
-    private readonly int _moveSpeed = 5;
-    private readonly float _visionLength = 10f;
-    private float _maxStopSeconds = 2f;
 
-    /// <summary>
-    /// Indicates this baddie is bound by ledges
-    /// </summary>
+    [Header("Ledge Bound Properties")]
+
     public bool LedgeBound = false;
-    /// <summary>
-    /// Used only by ledge bound baddies.
-    /// Tells the baddie when they have reached an edge and should turn back around
-    /// </summary>
     private bool _turnAround = true;
     /// <summary>
-    /// Used only ledge bound baddies.
+    /// Used only by ledge bound baddies.
     /// Indicates we have seen the target and should halt movement
     /// This allows a delay even after the target is out of collision
     /// sight contact so it looks like the baddie "knows" the target
@@ -33,18 +18,31 @@ public class Robot_GL1 : GroundBaddieLifeform {
     /// </summary>
     private float _alertTimeThreshold = -1f;
 
+    // These are the baddie specific values for properties that live on the parents
+    // This could be extracted out into configs but I don't mind them living here
+    // They're just used for initializing the property values that live on the 
+    // parent classes
+    private const float GRAVITY = -25.08f;
+    private const float HEALTH = 5f;
+    private const float SHOT_DELAY = 3f;
+    private const int MOVE_SPEED = 5;
+    private const float VISION_LENGTH = 10f;
+    private const float MAX_STOP_SECONDS = 2f;
+
+
+
     public void Start() {
         base.Start();
 
         // Set baddie specific data
-        GroundPositionData.ShotDelay = _shotDelay;
-        GroundPositionData.MoveSpeed = _moveSpeed;
+        GroundPositionData.ShotDelay = SHOT_DELAY;
+        GroundPositionData.MoveSpeed = MOVE_SPEED;
         GroundPositionData.MoveDirection = Vector2.left;
-        GroundPositionData.MaxStopSeconds = _maxStopSeconds;
+        GroundPositionData.MaxStopSeconds = MAX_STOP_SECONDS;
         GroundPositionData.FireDelay = 1f;
-        VisionRayLength = _visionLength;
-        Gravity = _gravity;
-        Health = _health;
+        VisionRayLength = VISION_LENGTH;
+        Gravity = GRAVITY;
+        Health = HEALTH;
     }
 
     public void Update() {
@@ -57,40 +55,24 @@ public class Robot_GL1 : GroundBaddieLifeform {
         // Find out where the target is in reference to this.
         var directionToTarget = transform.position.x - Target.position.x;
 
+        // Check if we can shoot at the target
+        var hCollider = FireRaycast();
+        if (hCollider.collider != null) {
+            _alertTimeThreshold = LedgeBound ? Time.time + 1f : 0f;
+            HandleCollision();
+        }
+
         if (LedgeBound) {
-            // Check if we can shoot at the target
-            var hCollider = FireRaycast();
-            if (hCollider.collider != null) {
-                _alertTimeThreshold = Time.time + 1f;
-                HandleCollision();
-            }
             // We previously saw the target but no longer see him.
             // Wait the max amount of time until going back to wandering
             if (_alertTimeThreshold >= Time.time) {
-                print("Ledge bound baddie previously spotted an enemy and has stopped moving to investigate");
                 Velocity = Vector2.zero;
                 return;
             }
-            // Update the way the baddie is facing
-            CalculateFacingDirection(GroundPositionData.MoveDirection.x*-1);
 
-            // Handle ledges
-            if (Controller2d.Collisions.NearLedge && _turnAround) {
-                print("Near ledge, prepare to turn around");
-                GroundPositionData.MoveDirection.x  = Vector2.right.x * (FacingRight ? -1f : 1f);
-                _turnAround = false;
-                Invoke("ResetTurnAround", 0.5f);
-            }
-            //Move the baddie
-            float targetVelocityX = GroundPositionData.MoveSpeed * GroundPositionData.MoveDirection.x;
-            Velocity.x = Mathf.SmoothDamp(Velocity.x, targetVelocityX, ref GroundPositionData.VelocityXSmoothing, Controller2d.Collisions.FromBelow ? AccelerationTimeGrounded : AccelerationTimeAirborne);
+            HandleLedgeboundMovement();
+
         } else {
-            // Check if we can shoot at the target
-            var hCollider = FireRaycast();
-            if (hCollider.collider != null) {
-                HandleCollision();
-            }
-
             // Face that direction
             CalculateFacingDirection(directionToTarget);
 
@@ -99,19 +81,27 @@ public class Robot_GL1 : GroundBaddieLifeform {
                 CalculateVelocity();
             }
         }
+
         Move();
     }
 
-    /// <summary>
-    /// Helper method to reset the turnAround variable by was of a coroutine
-    /// </summary>
+    private void HandleLedgeboundMovement() {
+        CalculateFacingDirection(GroundPositionData.MoveDirection.x * -1);
+
+        // Handle ledges
+        if (Controller2d.Collisions.NearLedge && _turnAround) {
+            GroundPositionData.MoveDirection.x = Vector2.right.x * (FacingRight ? -1f : 1f);
+            _turnAround = false;
+            Invoke("ResetTurnAround", 0.5f);
+        }
+        float targetVelocityX = GroundPositionData.MoveSpeed * GroundPositionData.MoveDirection.x;
+        Velocity.x = Mathf.SmoothDamp(Velocity.x, targetVelocityX, ref GroundPositionData.VelocityXSmoothing, Controller2d.Collisions.FromBelow ? AccelerationTimeGrounded : AccelerationTimeAirborne);
+    }
+
     private void ResetTurnAround() {
         _turnAround = true;
     }
 
-    /// <summary>
-    /// Calculate the velocity
-    /// </summary>
     private void CalculateVelocity() {
         float targetVelocityX = GroundPositionData.MoveSpeed * (FacingRight ? 1 : -1);
         Velocity.x = Mathf.SmoothDamp(Velocity.x, targetVelocityX, ref GroundPositionData.VelocityXSmoothing, Controller2d.Collisions.FromBelow ? AccelerationTimeGrounded : AccelerationTimeAirborne);
