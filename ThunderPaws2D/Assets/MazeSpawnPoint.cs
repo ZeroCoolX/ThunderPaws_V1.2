36ea5b5ea06d3798102305c3bb83900d5d259bfc
cnpatton <botanicality@gmail.com>
2018-09-03 12:49:11 -0500

    Adding Maze Logic

---
+++ ThunderPaws2D/Assets/MazeSpawnPoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeSpawnPoint : MonoBehaviour {
+
+    public Transform BaddiePrefab;
+
+    public int MaxBaddiesAllowed;
+    public int MinBaddiesAllowed;
+
+    private int _baddiesSpawned;
+
+    // Use this for initialization
+    void Start () {
+        var interval = 0f;
+        for (int i = 0; i < MaxBaddiesAllowed; ++i) {
+            Invoke("SpawnNewRoundOfBaddies", interval);
+            interval += 0.5f;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (_baddiesSpawned <= 0) {
+        //    for (int i = 0; i < MaxBaddiesAllowed; ++i) {
+        //        Invoke("SpawnNewRoundOfBaddies", 0.5f);
+        //    }
+        //}
+	}
+
+    private void SpawnNewRoundOfBaddies() {
+        ++_baddiesSpawned;
+        var clone = Instantiate(BaddiePrefab, transform.position, transform.rotation);
+        clone.GetComponent<SpriteRenderer>().color = Color.red;
+        clone.GetComponent<Robot_GL1>().Health = 100;
+        clone.GetComponent<Robot_GL1>().LedgeBound = true;
+        clone.GetComponent<Robot_GL1>().enabled = true;
+    }
+
+}
---
+++ ThunderPaws2D/Assets/MazeSpawner.cs
@@ -0,0 +1,22 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class MazeSpawner : MonoBehaviour {
+
+//    public Transform[] MazeLevels = new Transform[4];
+
+//    private Dictionary<string, Transform> _mazeLevelSpawnMap;
+
+//	// Use this for initialization
+//	void Start () {
+//        //_mazeLevelSpawnMap = new Dictionary<string, Transform>();
+//       // _mazeLevelSpawnMap.Add
+
+//    }
+
+//    // Update is called once per frame
+//    void Update () {
+		
+//	}
+//}
