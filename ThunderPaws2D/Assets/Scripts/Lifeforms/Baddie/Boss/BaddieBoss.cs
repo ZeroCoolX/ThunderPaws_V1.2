e257af27e4e4c20a5f9dc003c3e3ddeb02a86e9c
cnpatton <botanicality@gmail.com>
2018-09-11 21:26:13 -0500

    Added Vertical Heavy attack for boss

--- ThunderPaws2D/Assets/Scripts/Lifeforms/Baddie/Boss/BaddieBoss.cs
+++ ThunderPaws2D/Assets/Scripts/Lifeforms/Baddie/Boss/BaddieBoss.cs
@@ -3,25 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaddieBoss : BaddieLifeform {
+    public Transform[] Attack1Points;
 
-    //public Transform target;
-    //public float yTarget = 2;
-    //public float xTarget = 2;
+    [Header("Play Vertical Attack")]
+    public bool Vattack = false;
+    private bool _vAttackInitiated;
+    [Header("Play Horizontal Attack")]
+    public bool Hattack;
+
     private float smoothTime = 1F;
     private float yVelocity = 0.3F;
     private float xVelocity = 0.3F;
-
-    //void Update() {
-    //    float newPositiony = Mathf.SmoothDamp(transform.position.y, yTarget, ref yVelocity, smoothTime);
-    //    float newPositionx = Mathf.SmoothDamp(transform.position.x, xTarget, ref xVelocity, smoothTime);
-    //    transform.position = new Vector3(newPositionx, newPositiony, transform.position.z);
-    //}
-    public Transform[] Attack1Points;
-
     private Vector3 _currentAttackPoint;
     private float _moveSpeed = 1f;
     private float _moveTrigger;
     private float _delayBetweenMoves = 1f;
+
+    public delegate void VerticalHeavyAttackDelegate();
+    public VerticalHeavyAttackDelegate PlayVerticalHeavyAttack;
+    public Transform GetTarget() {
+        return Target;
+    }
 
 
     private new void Start() {
@@ -29,7 +31,15 @@ public class BaddieBoss : BaddieLifeform {
         Gravity = 0.0f;
         RandomlySelectAttackPoint();
         _moveTrigger = Time.time + _delayBetweenMoves;
+
+        transform.GetComponent<VerticalHeavyAttack>().OnComplete += ResumeBasicAttack;
     }
+
+    private void ResumeBasicAttack() {
+        Vattack = false;
+        _vAttackInitiated = false;
+    }
+
     // Update is called once per frame
     private new void Update() {
         base.Update();
@@ -41,6 +51,21 @@ public class BaddieBoss : BaddieLifeform {
         // Find out where the target is in reference to this.
         var directionToTarget = transform.position.x - Target.position.x;
         CalculateFacingDirection(directionToTarget);
+
+        if (Vattack && !_vAttackInitiated) {
+            _vAttackInitiated = true;
+            PlayVerticalHeavyAttack.Invoke();
+        }
+        if (_vAttackInitiated) {
+            return;
+        }
+
+        //if (Hattack) {
+        //    var rand = Random.Range(0, 2);
+        //    _currentAttackPoint = Attack3Points[rand].position;
+        //    CalculateVelocity();
+        //    return;
+        //}
 
         if (Time.time > _moveTrigger) {
             RandomlySelectAttackPoint();
@@ -60,6 +85,10 @@ public class BaddieBoss : BaddieLifeform {
         var rand = Random.Range(0, 5);
         print("Selecting random point: " + Attack1Points[rand].gameObject.name);
         _currentAttackPoint = Attack1Points[rand].position;
+    }
+
+    private void VerticalHeavyAttack() {
+
     }
 
 }
---
+++ ThunderPaws2D/Assets/VerticalHeavyAttack.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalHeavyAttack : MonoBehaviour {
+
+    public Transform AttackPoint;
+    public Transform MovementIndicator;
+
+    public delegate void AttackComplete();
+    public AttackComplete OnComplete;
+
+    private Transform _target;
+    private float vAttackTimeBeforeSmash = 5f;
+    private float smoothTime = 1F;
+    private float yVelocity = 0.3F;
+    private float xVelocity = 0.3F;
+    private Vector3 _currentAttackPoint;
+    private float _moveSpeed = 1f;
+    private float _moveTrigger;
+    private float _delayBetweenMoves = 1f;
+
+    private bool _attack;
+    private bool _smashLocked = false;
+    private bool _flashOn = false;
+
+    private enum AttackState { DEFAULT, RISE, TRACK, SMASH, PAUSE, END }
+    private AttackState _attackState = AttackState.DEFAULT;
+
+    private void Start() {
+        MovementIndicator.gameObject.SetActive(false);
+        var bossBaddieScript = transform.GetComponent<BaddieBoss>();
+        _target = bossBaddieScript.GetTarget();
+        bossBaddieScript.PlayVerticalHeavyAttack += InitiateAttack;
+    }
+
+    private void InitiateAttack() {
+        if (!_attack) {
+            _attack = true;
+            _attackState = AttackState.RISE;
+        }
+    }
+
+    private void ResetState() {
+        _attack = false;
+        _smashLocked = false;
+        _attackState = AttackState.DEFAULT;
+    }
+
+    private void Update() {
+        switch (_attackState) {
+            case AttackState.RISE:
+                _currentAttackPoint = AttackPoint.position;
+                StartCoroutine(ChangeStateAfterSeconds(AttackState.TRACK, 2f));
+                break;
+            case AttackState.TRACK:
+                if (!MovementIndicator.gameObject.activeSelf) {
+                    MovementIndicator.gameObject.SetActive(true);
+                }
+                _currentAttackPoint.x = transform.GetComponent<BaddieBoss>().GetTarget().position.x;
+                MovementIndicator.transform.position = new Vector3(_currentAttackPoint.x, MovementIndicator.transform.position.y, MovementIndicator.transform.position.z);
+                StartCoroutine(ChangeStateAfterSeconds(AttackState.PAUSE, 5f));
+                break;
+            case AttackState.SMASH:
+                if(!_smashLocked) {
+                    MovementIndicator.gameObject.SetActive(false);
+                    _currentAttackPoint = new Vector3(_currentAttackPoint.x, transform.GetComponent<BaddieBoss>().GetTarget().position.y + 1, _currentAttackPoint.z);
+                    _smashLocked = true;
+                }
+                smoothTime = 0.3f;
+                StartCoroutine(ChangeStateAfterSeconds(AttackState.END, 1f));
+                break;
+            case AttackState.PAUSE:
+                Flash();
+                StartCoroutine(ChangeStateAfterSeconds(AttackState.SMASH, 0.5f));
+                break;
+            case AttackState.END:
+                ResetState();
+                OnComplete.Invoke();
+                break;
+            case AttackState.DEFAULT:
+            default:
+                return;
+        }
+        CalculateVelocity();
+    }
+
+    private void Flash() {
+        if (_flashOn) {
+            MovementIndicator.GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0.8f);
+            _flashOn = false;
+        } else {
+            MovementIndicator.GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0f);
+            _flashOn = true;
+        }
+    }
+
+    private IEnumerator ChangeStateAfterSeconds(AttackState state, float afterSeconds) {
+        yield return new WaitForSeconds(afterSeconds);
+        _attackState = state;
+    }
+
+    private void CalculateVelocity() {
+        float newX = Mathf.SmoothDamp(transform.position.x, _currentAttackPoint.x, ref yVelocity, smoothTime);
+        float newY = Mathf.SmoothDamp(transform.position.y, _currentAttackPoint.y, ref xVelocity, smoothTime);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+}
