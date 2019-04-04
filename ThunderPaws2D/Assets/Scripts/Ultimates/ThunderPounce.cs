587de1c4375df6941e5aec461abc8b732c218a95
cnpatton <botanicality@gmail.com>
2019-04-04 18:39:23 -0500

    Added thunderpounce ult art

--- ThunderPaws2D/Assets/Scripts/Ultimates/ThunderPounce.cs
+++ ThunderPaws2D/Assets/Scripts/Ultimates/ThunderPounce.cs
@@ -1,8 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class ThunderPounce : Ultimate {
+    public Animator PounceEffectAnimator;
+    public Transform Weapon;
+
+    public Transform BlastRadiusEffectPrefab;
+    private Animator _blastRadiusEffectAnimator;
+    private Transform _slashEffect;
 
     private List<GameObject> _baddies;
     private float _pounceHeightMax = 8F;
@@ -18,6 +25,8 @@ public class ThunderPounce : Ultimate {
 
     public override void Activate() {
         print("ThunderPounce activated!");
+        Weapon.gameObject.SetActive(false);
+
         PlayerStats.UltEnabled = true;
         PlayerStats.UltReady = false;
 
@@ -26,10 +35,33 @@ public class ThunderPounce : Ultimate {
         ResetCollection();
         CollectAllBaddies();
 
-        _pounceApex = transform.position + (Vector3.up * _pounceHeightMax);
-        _originPosition = transform.position;
+        //_pounceApex = transform.position + (Vector3.up * _pounceHeightMax);
+        //_originPosition = transform.position;
+        //_active = true;
 
-        _active = true;
+        PounceEffectAnimator.SetBool("thunderpounce_jump", true);
+        PounceEffectAnimator.SetBool("thunderpounce_spin", true);
+        StartCoroutine(End());
+    }
+
+    IEnumerator End() {
+        yield return new WaitForSeconds(1);
+        PounceEffectAnimator.SetBool("thunderpounce_smash", true);
+        Invoke("ResetAnimation", 0.25f);
+        DamageAllBaddies();
+        ResetCollection();
+        DeactivateDelegate.Invoke();
+    }
+
+    void ResetAnimation() {
+        //var bounds = transform.GetComponent<BoxCollider2D>().bounds;
+        //var blastPos = new Vector3(transform.position.x, transform.position.y - bounds.min.y, transform.position.z);
+        //var left = Instantiate(BlastRadiusEffectPrefab, blastPos, transform.rotation, transform) as Transform;
+        //var rightEffect = Instantiate(BlastRadiusEffectPrefab, blastPos, transform.rotation, transform) as Transform;
+        PounceEffectAnimator.SetBool("thunderpounce_jump", false);
+        PounceEffectAnimator.SetBool("thunderpounce_spin", false);
+        PounceEffectAnimator.SetBool("thunderpounce_smash", false);
+        Weapon.gameObject.SetActive(true);
     }
 
     private void CollectAllBaddies() {
@@ -70,6 +102,8 @@ public class ThunderPounce : Ultimate {
             DamageAllBaddies();
             ResetCollection();
             DeactivateDelegate.Invoke();
+            PounceEffectAnimator.SetBool("thunderpounce_smash", false);
+            PounceEffectAnimator.SetBool("thunderpounce_jump", false);
             _active = false;
             return;
         }
